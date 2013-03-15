dbda5b42c3553a7c6d92eafd39e0bd15d07d6e03
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2013-03-15 11:37:58 +0200

    sample component datas are removed

--- ZulaLibrary/UserInterface/Pages/ContentPage/Model/SMContentPage.m
+++ ZulaLibrary/UserInterface/Pages/ContentPage/Model/SMContentPage.m
@@ -77,30 +77,4 @@
     }];
 }
 
-+ (void)fetchWithCompletionSample:(void (^)(SMContentPage *, NSError *))completion
-{
-    // sample images:
-    // http://folioflip.foliothemes.com/wp-content/uploads/2009/11/guitar2-320x150.jpg
-    // http://www.marcjulienhomes.com/wp-content/uploads/2011/03/0040-320x150.jpg
-    // http://www.emobilez.com/wallpapers/data/media/298/nuclear_iphone_wallpapers.jpg
-    // http://www.dailymobile.net/wp-content/uploads/2009/02/iphone-wallpaper-087.jpg
-    // http://www.c4rlh.com/mobile-phone/free-wallpapers/signs/cold-apple-logo.jpg
-    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:
-                                @"Sample Title", @"title",
-                                @"http://folioflip.foliothemes.com/wp-content/uploads/2009/11/guitar2-320x150.jpg", @"image",
-                                @"http://www.c4rlh.com/mobile-phone/free-wallpapers/signs/cold-apple-logo.jpg", @"bg_image",
-                                @"<html><body style='color:white;padding:none;margin:none;'>Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.</body></html>", @"text",
-                                nil];
-    SMContentPage *contentPage = [[SMContentPage alloc] initWithAttributes:attributes];
-    
-    double delayInSeconds = 1.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        if (completion) {
-            completion(contentPage, nil);
-        }
-    });
-    
-}
-
 @end
