96e4953569416f3af24c5e798678c00c764a7b87
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2013-05-21 20:36:05 +0300

    submenu is done

---
+++ ZulaLibrary/UserInterface/Pages/ContentContainer/View/SMTriangle.h
@@ -0,0 +1,13 @@
+//
+//  Triangle.h
+//  ZulaLibrary
+//
+//  Created by Suleyman Melikoglu on 5/21/13.
+//  Copyright (c) 2013 laplacesdemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Triangle : UIView
+
+@end
---
+++ ZulaLibrary/UserInterface/Pages/ContentContainer/View/SMTriangle.m
@@ -0,0 +1,31 @@
+//
+//  Triangle.m
+//  ZulaLibrary
+//
+//  Created by Suleyman Melikoglu on 5/21/13.
+//  Copyright (c) 2013 laplacesdemon. All rights reserved.
+//
+
+#import "Triangle.h"
+
+@implementation Triangle
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
